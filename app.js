2ffc3db21f472e6e88ccb289eb2721bc12dfce0c
Asif Hussain <asifhussain.loop@gmail.com>
2025-04-28 21:11:44 +0600

    JS FIRST CODE

---
+++ app.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 15; i++) {
+  if (i % 5 === 0) {
+    console.log(`${i} - "ASAP Frontend"`);
+  }
+
+ else if (i % 2 === 0) {
+    console.log(`${i} - "Frontend"`);
+  }
+  else 
+  console.log(`${i} - "ASAP"`);
+}
